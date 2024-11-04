6023a4f5beb2c966e3b5c5996b3864be101f68d2
Daniil Leshchev <leshev.da@gmail.com>
2024-11-04 18:28:19 +0500

    remove unnecessary comments and add docstrings

--- pseudocode.py
+++ pseudocode.py
@@ -3,16 +3,20 @@
 
 import os
 from dotenv import load_dotenv
-from supabase import *
+from supabase import create_client
 
-def get_common_tags(tags1, tags2): # вынес формирование общих тегов в отдельную функцию
+
+def get_common_tags(tags1, tags2):
+    """Получение общих тегов из двух списков с тегами."""
     common_tags = set(tags1).intersection(tags2)
     return list(common_tags), len(common_tags)
 
+
 def get_linking_profession_to_competency(profession, data_of_competencies):
+    """Получение связанных профессий и компетенций."""
     matched_competencies = []
     for competency in data_of_competencies:
-        common_tags, weight = get_common_tags(profession[0]['tags'], competency['tags']) # везде было с ['tags'] а тут нет
+        common_tags, weight = get_common_tags(profession[0]['tags'], competency['tags'])
         if common_tags:
             matched_competencies.append(
                 {
@@ -24,9 +28,11 @@ def get_linking_profession_to_competency(profession, data_of_competencies):
             )
     return matched_competencies
 
+
 def get_linking_competency_to_course(competency, list_of_courses):
+    """Получение связанных компетенций и курсов."""
     matched_courses = []
-    for course in list_of_courses: # поменял переменную на course
+    for course in list_of_courses:
         common_tags, weight = get_common_tags(competency['tags'], course['tags'])
         if common_tags:
             matched_courses.append(
@@ -41,31 +47,38 @@ def get_linking_competency_to_course(competency, list_of_courses):
                     'competency': competency,
                     'tags': course['tags'],
                     'weight': weight
-                } # Добавлены недостающие атрибуты
+                }
             )
     return matched_courses
 
-def recommend_courses(profession, competencies, courses, discipline=None): # по умолчанию будем обозначать, что фильтра нет
+
+def recommend_courses(profession, competencies, courses, discipline=None):
+    """Рекомендация курсов."""
     matched_competencies = get_linking_profession_to_competency(profession, competencies)
     all_matched_courses = []
     for competency in matched_competencies:
         matched_courses = get_linking_competency_to_course(competency, courses)
 
-        if discipline: # если был передан фильтр(на данный момент на принадлежность курсов дисциплине)
-            matched_courses = list(filter(lambda x: x['discipline_id'] == discipline, matched_courses)) # проверяем, что в данной дисциплине есть ссылка на id курса
+        if discipline:  # если был передан фильтр принадлежности курсов дисциплине
+            matched_courses = list(filter(lambda x: x['discipline_id'] == discipline, matched_courses))
         for match_course in matched_courses:
             match_course['weight'] += competency['weight']
         all_matched_courses.extend(matched_courses)
     return sorted(all_matched_courses, key=lambda x: x['weight'], reverse=True)
 
+
 def recommend_courses_for_profession(profession, competencies, courses):
+    """Рекомендация курсов для профессии."""
     return recommend_courses(profession, competencies, courses)
 
-# В рамках дисциплины должны выбрать наиболее подходящий из связанных с ней курсов
+
 def recommend_course_for_discipline(profession, competencies, courses, discipline):
+    """Рекомендация курсов в рамках дисциплины."""
     return recommend_courses(profession, competencies, courses, discipline=discipline)
 
-def init_db(): # Инициализация БД
+
+def init_db():
+    """Инициализация БД."""
     load_dotenv()
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
@@ -129,6 +142,7 @@ def main():
     print(f'Дисциплина: {discipline[0]['name']}')
     for discipline_course in discipline_courses:
         print(f"Курс: {discipline_course['name']} в рамках компетенции {discipline_course['competency']['name']} с весом {discipline_course['weight']}")
+
 
 if __name__ == '__main__':
     main()
