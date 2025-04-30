4d1a3bdc3bd6d5fd69310755ca07847819bfeb47
Daniil Leshchev <leshev.da@gmail.com>
2025-04-30 19:50:47 +0500

    professions list and detail endpoints

--- urfuture/professions/views.py
+++ urfuture/professions/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Profession
+from .serializers import ProfessionSerializer, ProfessionDetailSerializer
 
-# Create your views here.
+
+class ProfessionListAPIView(generics.ListAPIView):
+    queryset = Profession.objects.all()
+    serializer_class = ProfessionSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ProfessionDetailAPIView(generics.RetrieveAPIView):
+    queryset = Profession.objects.all()
+    serializer_class = ProfessionDetailSerializer
+    permission_classes = (permissions.AllowAny,)
--- urfuture/urfuture/urls.py
+++ urfuture/urfuture/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/professions/', include('professions.urls')),
     path('api/',  include('users.urls'))
 ]
