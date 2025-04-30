c00db413b2082b92a52a2f540572a41c79134467
Daniil Leshchev <leshev.da@gmail.com>
2025-04-30 17:39:26 +0500

    login with jwt authentication

--- urfuture/urfuture/urls.py
+++ urfuture/urfuture/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/',  include('users.urls'))
+    path('api/',  include('users.urls'))
 ]
--- urfuture/users/views.py
+++ urfuture/users/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, EmailTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
+
+
+class LoginAPIView(TokenObtainPairView):
+    serializer_class = EmailTokenObtainPairSerializer
