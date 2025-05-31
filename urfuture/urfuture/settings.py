89a6e2a7124ab71a37176472b0ee82e4936aac51
Daniil Leshchev <leshev.da@gmail.com>
2025-06-01 01:32:25 +0500

    update cors origins

--- urfuture/urfuture/settings.py
+++ urfuture/urfuture/settings.py
@@ -222,5 +222,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5175",
+    "http://127.0.0.1:5175",
     "http://app.urfuture.tech"
 ]
