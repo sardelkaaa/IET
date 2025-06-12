f066e72246ac41d028eeb93b6b72eb053587a7e2
Daniil Leshchev <leshev.da@gmail.com>
2025-06-12 21:47:56 +0500

    update courses pagination

--- urfuture/courses/views.py
+++ urfuture/courses/views.py
@@ -9,7 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CoursesPagination(PageNumberPagination):
-    page_size = 15
+    page_size = 9
     page_size_query_param = 'page_size'
     max_page_size = 30
 
