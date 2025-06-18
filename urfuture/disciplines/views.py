39f5e9cf44b2136c226ec6b132813b798ba23028
Daniil Leshchev <leshev.da@gmail.com>
2025-06-18 18:55:42 +0500

    add group by semester for disciplines endpoint

--- urfuture/disciplines/views.py
+++ urfuture/disciplines/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import DisciplinesDirections
 from .serializers import DisciplineSerializer
 from api_docs.disciplines import disciplines_list_doc
+from rest_framework.response import Response
 
 
 @disciplines_list_doc
@@ -19,3 +20,24 @@ class MyDirectionDisciplinesAPIView(generics.ListAPIView):
         if semester:
             qs = qs.filter(semester=semester)
         return qs.select_related('discipline').order_by('semester')
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+
+        grouped = {}
+        for item in data:
+            sem = item['semester']
+            entry = {
+                'id': item['id'],
+                'name': item['name']
+            }
+            grouped.setdefault(sem, []).append(entry)
+
+        result = [
+            {'semester': sem, 'courses': courses}
+            for sem, courses in sorted(grouped.items(), key=lambda x: x[0])
+        ]
+
+        return Response(result)
